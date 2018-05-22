7fe5272c43ab380e779516eaa5ecd7ed5a7a2f3e
Ben Simms <ben@bensimms.moe>
2018-05-22 11:45:00 +0100

    Finish up dsl

--- src/cdsl.rs
+++ src/cdsl.rs
@@ -1,6 +1,4 @@
-use std::{
-    rc::Rc,
-};
+use std::rc::Rc;
 
 pub enum CExpr {
     BinOp {
@@ -32,7 +30,7 @@ pub enum CExpr {
 }
 
 pub enum CType {
-    Ptr { to: Rc<CType>, },
+    Ptr { to: Rc<CType> },
     Arr { of: Rc<CType>, len: usize },
     Int { size: usize, sign: bool },
     Struct { name: String },
@@ -40,11 +38,6 @@ pub enum CType {
 }
 
 pub enum CStmt {
-    Fun {
-        name: String,
-        args: Vec<(String, Rc<CType>)>,
-        body: Vec<Rc<CStmt>>,
-    },
     IF {
         cond: Rc<CExpr>,
         body: Rc<CStmt>,
@@ -63,6 +56,27 @@ pub enum CStmt {
     Expr(Rc<CExpr>),
 }
 
+pub enum CDecl {
+    Fun {
+        name: String,
+        typ: Rc<CType>,
+        args: Vec<(String, Rc<CType>)>,
+        body: Vec<Rc<CStmt>>,
+    },
+    Struct {
+        name: String,
+        members: Vec<(String, Rc<CType>)>,
+    },
+    Union {
+        name: String,
+        members: Vec<(String, Rc<CType>)>,
+    },
+    Var {
+        name: String,
+        typ: Rc<CType>,
+        init: Option<Rc<CExpr>>,
+    },
+}
 
 pub trait ToC {
     fn export_internal(&self, s: &mut String);
@@ -77,6 +91,7 @@ macro_rules! export_helper {
     ($s:ident, str $e:expr) => ( $s.push_str($e) );
     ($s:ident, chr $e:expr) => ( $s.push($e) );
     ($s:ident, exp $e:ident) => ( $e.export_internal(&mut $s) );
+    ($s:ident, vec $e:expr) => ( for elem in $e { elem.export_internal(&mut $s); } );
     ($s:ident, $cmd:tt $op:tt $(, $innercmd:tt $innerop:tt)*) => {{
         export_helper!($s, $cmd $op);
         export_helper!($s, $($innercmd $innerop),*);
@@ -87,30 +102,143 @@ impl ToC for CExpr {
     fn export_internal(&self, mut s: &mut String) {
         use self::CExpr::*;
 
-        match &self {
-            BinOp {op, left, right} =>
+        match self {
+            BinOp { op, left, right } => {
                 export_helper!(s, chr '(', exp left, chr ')', str op, chr '(', exp right, chr ')')
-            ,
-            PreUnOp {op, ex} => export_helper!(s, chr '(', exp ex, chr ')', str op),
-            PostUnOp {op, ex} => export_helper!(s, str op, chr '(', exp ex, chr ')'),
-            ArrIndexOp {index, expr} => export_helper!(s, chr '(', exp expr, str ")[", exp index, chr ')'),
-            FunCallOp {expr, ands} => {
+            }
+            PreUnOp { op, ex } => export_helper!(s, chr '(', exp ex, chr ')', str op),
+            PostUnOp { op, ex } => export_helper!(s, str op, chr '(', exp ex, chr ')'),
+            ArrIndexOp { index, expr } => {
+                export_helper!(s, chr '(', exp expr, str ")[", exp index, chr ')')
+            }
+            FunCallOp { expr, ands } => {
                 export_helper!(s, chr '(', exp expr, str ")(");
 
-                let len = ands.len();
+                let mut it = ands.iter();
 
-                for expr in &ands[..len-1] {
+                if let Some(expr) = it.next() {
                     expr.export_internal(&mut s);
-                    s.push(',');
                 }
-                if let Some(last) = ands.last() {
-                    last.export_internal(&mut s);
+
+                for expr in it {
+                    s.push(',');
+                    expr.export_internal(&mut s);
                 }
 
                 export_helper!(s, chr ')');
-            },
-            Cast {ex, typ} => panic!("not done yet"), // export_helper!(s, chr '(', exp typ , str ")(", exp ex, chr ')');
+            }
+            Cast { ex, typ } => export_helper!(s, chr '(', exp typ , str ")(", exp ex, chr ')'),
             Lit(lit) => export_helper!(s, chr '(', str lit, chr ')'),
+        }
+    }
+}
+
+impl ToC for CType {
+    fn export_internal(&self, s: &mut String) {
+        s.push_str(&self.export_with_name(""));
+    }
+}
+
+impl CType {
+    fn export_with_name(&self, name: &str) -> String {
+        use self::CType::*;
+
+        let mut typ = Some(self);
+        let mut gen = name.to_owned();
+
+        while let Some(typ_o) = typ.take() {
+            gen = match typ_o {
+                Ptr {..} => format!("*{}", gen),
+                Arr {of: _, len} => format!("({})[{}]", gen, len),
+                Int {size, sign} => {
+                    let name = format!("{}int{}_t",
+                                       if *sign { "u" } else { "" },
+                                       size);
+                    format!("{} {}", name, gen)
+                },
+                Struct {name} => format!("struct {} {}", name, gen),
+                Union {name} => format!("union {} {}", name, gen),
+            };
+
+            match typ_o {
+                Ptr {to} => typ = Some(to),
+                Arr {of, ..} => typ = Some(of),
+                _ => (),
+            };
+        }
+
+        return gen;
+    }
+}
+
+impl ToC for CStmt {
+    fn export_internal(&self, mut s: &mut String) {
+        use self::CStmt::*;
+
+        match self {
+            IF {cond, body} => export_helper!(s, str "if (", exp cond, chr ')', exp body),
+            While {cond, body} => export_helper!(s, str "while (", exp cond, chr ')', exp body),
+            For {init, test, updt, body} => export_helper!(s, str "for (", exp init, chr ';', exp test, chr ';', exp updt, chr ')', exp body),
+            Block(body) => export_helper!(s, chr '{', vec body, chr '}'),
+            Expr(body) => export_helper!(s, exp body, chr ';'),
+        }
+    }
+}
+
+impl ToC for CDecl {
+    fn export_internal(&self, mut s: &mut String) {
+        use self::CDecl::*;
+
+        match self {
+            Fun {name, typ, args, body} => {
+                let mut f = String::new();
+
+                f.push_str(&name);
+                f.push('(');
+
+                let mut it = args.iter();
+
+                if let Some((aname, atyp)) = it.next() {
+                    f.push_str(&atyp.export_with_name(aname));
+                }
+
+                for (aname, atyp) in it {
+                    s.push(',');
+                    f.push_str(&atyp.export_with_name(aname));
+                }
+
+                f.push(')');
+
+                s.push_str(&typ.export_with_name(&f));
+
+                export_helper!(s, chr '{', vec body, chr '}');
+            },
+            Struct {name, members} => {
+                export_helper!(s, str "struct ", str name, chr '{');
+
+                for (aname, atyp) in members {
+                    s.push_str(&atyp.export_with_name(aname));
+                    s.push(';');
+                }
+                s.push(';');
+            },
+            Union {name, members} => {
+                export_helper!(s, str "union ", str name, chr '{');
+
+                for (aname, atyp) in members {
+                    s.push_str(&atyp.export_with_name(aname));
+                    s.push(';');
+                }
+                s.push(';');
+            },
+            Var {name, typ, init} => {
+                s.push_str(&typ.export_with_name(name));
+                if let Some(init) = init {
+                    export_helper!(s, exp init);
+                }
+                s.push(';');
+            }
+
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::rc::Rc;
 use cdsl::*;
 
 fn main() {
-    let op = CExpr::BinOp {
-        op: "+".to_owned(),
-        left: Rc::new(CExpr::Lit("wew".to_owned())),
-        right: Rc::new(CExpr::Lit("lad".to_owned())),
+    let fn_ = CDecl::Fun {
+        name: "lol".to_owned(),
+        typ: Rc::new(CType::Ptr { to: Rc::new(CType::Arr { of: Rc::new(CType::Int { size: 8, sign: false}),
+                                                           len: 10})}),
+        args: vec![("a1".to_owned(), Rc::new(CType::Ptr { to: Rc::new( CType::Int { size: 16, sign: false} )}))],
+        body: vec![Rc::new(CStmt::Expr(Rc::new(CExpr::Lit("lol".to_owned()))))],
     };
 
-    println!("{}", op.export());
+    println!("{}", fn_.export());
 }
