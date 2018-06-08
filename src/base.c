e9226b3f59ead7e41599772ace5a7ca95977bf40
Ben Simms <ben@bensimms.moe>
2018-06-08 12:40:08 +0100

    Design base a bit

--- src/base.c
+++ src/base.c
@@ -1,31 +1,87 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define RUNTIME_ERROR(S) printf("Runtime Error: %s\n", (S))
+
+enum object_tag {
+    CLOSURE = 0,
+};
+
+struct object {
+    enum object_tag tag;
+};
 
 struct env_elem {
-    const char *ident;     // owned by env_elem
+    size_t ident_id;
     struct object *val;    // shared
     struct env_elem *next; // owned by env_elem
 };
 
 struct closure {
-    void (*fn)(struct object *, struct closure *);
+    struct object base;
+    bool size; // false = 1 arg, true = 2 arg
+    union {
+        void (*fn_1)(struct object *, struct env_elem *);
+        void (*fn_2)(struct object *, struct object *, struct env_elem *);
+    };
     struct env_elem *env;
 };
 
-#define ADD_ENV(IDENT, VAL, HEAD)                                   \
-    do {                                                            \
-        struct env_elem *new_env = alloca(sizeof(struct env_elem)); \
-                                                                    \
-        size_t ident_len = strlen(IDENT) + 1;                       \
-                                                                    \
-        new_env->indent = alloca(ident_len);                        \
-        strcpy(new_env->ident, IDENT);                              \
-                                                                    \
-        new_env->val = VAL;                                         \
-        new_env->next = HEAD;                                       \
-                                                                    \
-        HEAD = new_env;                                             \
-    } while (0)                                                     \
+struct env_table_entry {
+    size_t env_id;
+    size_t *var_ids;
+};
 
-void lol() {
-    struct en;
+extern struct env_table_entry env_table[];
+
+#define ADD_ENV(IDENT, VAL, HEAD)                                       \
+    do {                                                                \
+        struct env_elem *new_env = alloca(sizeof(struct env_elem));     \
+                                                                        \
+        new_env->ident_id = (IDENT);                                    \
+        new_env->val = (VAL);                                           \
+        new_env->next = (HEAD);                                         \
+                                                                        \
+        (HEAD) = new_env;                                               \
+    } while (0)                                                         \
+
+void call_closure_one(struct object *rator, size_t rand_id, struct object *rand) {
+    if (rator->tag != 0) {
+        RUNTIME_ERROR("Called object was not a closure");
+    }
+
+    struct closure *closure = (struct closure *)rator;
+
+    if (closure->size != false) {
+        RUNTIME_ERROR("Called a closure that takes two args with one arg");
+    }
+
+    ADD_ENV(rand_id, rand, closure->env);
+    closure->fn_1(rand, closure->env);
+}
+
+void call_closure_two(struct object *rator, size_t rand_id, struct object *rand, size_t cont_id, struct object *cont) {
+    if (rator->tag != 0) {
+        RUNTIME_ERROR("Called object was not a closure");
+    }
+
+    struct closure *closure = (struct closure *)rator;
+
+    if (closure->size != true) {
+        RUNTIME_ERROR("Called a closure that takes two args with one arg");
+    }
+
+    ADD_ENV(rand_id, rand, closure->env);
+    ADD_ENV(cont_id, cont, closure->env);
+
+    closure->fn_2(rand, cont, closure->env);
+}
+
+void halt_func(struct object *cont, struct env_elem *env) {
+    (void)cont; // mmh
+    (void)env;
+    printf("Halt");
+    exit(0);
 }
